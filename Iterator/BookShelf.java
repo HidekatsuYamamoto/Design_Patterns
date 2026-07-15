0fed3b6dc3fd9748a341310a7539361930018f5a
Hidekatsu Yamamoto <yokan@dk9.so-net.ne.jp>
2026-07-15 10:10:41 +0900

    7/15

--- Iterator/BookShelf.java
+++ Iterator/BookShelf.java
@@ -1,17 +1,23 @@
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate {
-    private Book[] books; // 本の配列
+    // private Book[] books; // 本の配列
+    private ArrayList<Book> new_books;
     private int last = 0;
 
     public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+        // this.books = new Book[maxsize];
+        this.new_books = new ArrayList<Book>();
     }
 
     public Book getBookAt(int index) {
-        return this.books[index];
+        // return this.books[index];
+        return this.new_books.get(index);
     }
 
     public int appendBook(Book book) {
-        this.books[last] = book;
+        this.new_books.add(book);
+        // this.books[last] = book;
         return this.last++;
     }
 
--- Iterator/Main.java
+++ Iterator/Main.java
@@ -8,6 +8,18 @@ public class Main {
         bookshelf.appendBook(new Book("B"));
         bookshelf.appendBook(new Book("C"));
         bookshelf.appendBook(new Book("D"));
+        bookshelf.appendBook(new Book("E"));
+        bookshelf.appendBook(new Book("F"));
+        bookshelf.appendBook(new Book("G"));
+        bookshelf.appendBook(new Book("H"));
+        bookshelf.appendBook(new Book("I"));
+        bookshelf.appendBook(new Book("J"));
+        bookshelf.appendBook(new Book("K"));
+        bookshelf.appendBook(new Book("L"));
+        bookshelf.appendBook(new Book("M"));
+        bookshelf.appendBook(new Book("N"));
+        bookshelf.appendBook(new Book("O"));
+        bookshelf.appendBook(new Book("P"));
 
         Iterator it = bookshelf.iterator();
         // BookShelfIteratorのインスタンスをゲット。共通のIterator型にすることで共通化できる。
